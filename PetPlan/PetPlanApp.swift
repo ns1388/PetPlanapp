7568f62043cbaa9031527be562afdf2a700da5c9
Aanand Aggarwal <aanand.aggarwal@gmail.com>
2023-03-11 13:11:04 -0500

    Initial Commit

---
+++ PetPlan/PetPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPlanApp.swift
+//  PetPlan
+//
+//  Created by Aanand Aggarwal on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PetPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
