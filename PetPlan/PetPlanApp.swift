fd8d4919494dede759c079c5dfdc6e016a994464
aanandaggarwal <aanand.aggarwal@gmail.com>
2023-03-24 18:41:45 -0400

    test

--- PetPlan/PetPlanApp.swift
+++ PetPlan/PetPlanApp.swift
@@ -7,11 +7,68 @@
 
 import SwiftUI
 
-@main
-struct PetPlanApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct LoginPage: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        ZStack {
+            Color(.white)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Image(systemName: "pawprint")
+                    .font(.system(size: 80))
+                    .foregroundColor(.green)
+                
+                VStack(spacing: 16) {
+                    TextField("Email", text: $email)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .disableAutocorrection(true)
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                }
+                
+                Button(action: {
+                    // Log in action
+                }) {
+                    Text("Log in")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(8)
+                }
+                
+                HStack {
+                    Button(action: {
+                        // Register action
+                    }) {
+                        Text("Register")
+                            .font(.subheadline)
+                            .foregroundColor(.green)
+                            .underline()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // Reset password action
+                    }) {
+                        Text("Reset password")
+                            .font(.subheadline)
+                            .foregroundColor(.green)
+                            .underline()
+                    }
+                }
+            }
+            .padding()
         }
     }
 }
